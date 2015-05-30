433ce33a49e543781dcfe90837c80a556fcf82b2
Joseph Roque <github@josephroque.ca>
2015-05-30 09:49:02 -0400

    Setup divs for organizing content

--- js/site.js
+++ js/site.js
@@ -1,5 +1,3 @@
 function setPageAttributes() {
-	var colors = ["#ff0000", "#00ff00", "#0000ff"];
-	var random_color = colors[Math.floor(Math.random() * colors.length)];
-	document.getElementById('title').style.color = random_color;
+	
 }
