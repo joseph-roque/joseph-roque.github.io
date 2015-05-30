0d871ff3bf1e99045fe4699698c412f18dc06180
Joseph Roque <github@josephroque.ca>
2015-05-29 20:41:01 -0400

    Merging all pages into one

---
+++ js/site.js
@@ -0,0 +1,5 @@
+function setPageAttributes() {
+	var colors = ["#ff0000", "#00ff00", "#0000ff"];
+	var random_color = colors[Math.floor(Math.random() * colors.length)];
+	document.getElementById('title').style.color = random_color;
+}
