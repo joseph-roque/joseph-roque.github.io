1fdff6ebf2b3fa76c9d1d49d6fa16fa28f3b8a5e
Joseph Roque <github@josephroque.ca>
2015-05-31 13:24:25 -0400

    Testing js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
-function setPageAttributes() {
-	
-}
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
