0bc06f8fabfa4cec6227ee0e90498a06a6b7f32c
Joseph Roque <github@josephroque.ca>
2015-06-20 11:42:10 -0400

    Fixing mobile navigation, custom pages for work content

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	    if( target.length ) {
 	        event.preventDefault();
 	        $('html, body').animate({
-	            scrollTop: target.offset().top - 72
+	            scrollTop: target.offset().top
 	        }, 1000);
 	    }
 	});
