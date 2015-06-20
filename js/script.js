0f61b6bd47d74f0f736fd1a42aa22251c8a29edf
Joseph Roque <github@josephroque.ca>
2015-06-19 23:37:19 -0400

    Work and contact setup for content

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	    if( target.length ) {
 	        event.preventDefault();
 	        $('html, body').animate({
-	            scrollTop: target.offset().top
+	            scrollTop: target.offset().top - 72
 	        }, 1000);
 	    }
 	});
